7c983ae1bf28d5ab8977df210258f9d362ee4bef
Benny Tavarez <bennytavarez@hotmail.com>
2024-03-13 16:38:53 -0400

    added margin so the text arent so close to the border.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import ReactSwitch from "react-switch";
 
 export function Navbar(){
     return(<>
-    &nbsp; &nbsp; <Link to='/' className="link">home</Link>&nbsp; 
+    <Link to='/' className="link">home</Link>&nbsp; 
     &nbsp; <Link to='/sellers' className="link">sellers page</Link>&nbsp; &nbsp; 
     <Link to='/products' className="link">products page</Link>
     </>)
